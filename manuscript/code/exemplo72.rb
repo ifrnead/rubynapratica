fcaf63099c946785aed8611140d292028058230c
Jalerson Lima <jalerson@gmail.com>
2017-02-24 16:12:42 -0300

    Transcrevendo Aula 07

---
+++ manuscript/code/exemplo72.rb
@@ -0,0 +1,6 @@
+puts "Digite a sua idade:"
+idade = gets.chomp.to_i
+
+if idade >= 18
+  puts "Você é maior de idade!"
+end
---
+++ manuscript/code/exemplo76.rb
@@ -0,0 +1,4 @@
+puts "Digite a sua idade:"
+idade = gets.chomp.to_i
+
+puts "Você é maior de idade!" if idade >= 18

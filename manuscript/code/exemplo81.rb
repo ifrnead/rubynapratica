455d4404c8ae1069eb0753824e8c7d6c10a8ae2f
Jalerson Lima <jalerson@gmail.com>
2017-04-01 10:54:28 -0300

    Transcrevendo a Aula 08.

---
+++ manuscript/code/exemplo81.rb
@@ -0,0 +1,13 @@
+puts "Digite um número: "
+numero = gets.chomp.to_i
+
+puts "#{numero} x 1 = #{numero * 1}"
+puts "#{numero} x 2 = #{numero * 2}"
+puts "#{numero} x 3 = #{numero * 3}"
+puts "#{numero} x 4 = #{numero * 4}"
+puts "#{numero} x 5 = #{numero * 5}"
+puts "#{numero} x 6 = #{numero * 6}"
+puts "#{numero} x 7 = #{numero * 7}"
+puts "#{numero} x 8 = #{numero * 8}"
+puts "#{numero} x 9 = #{numero * 9}"
+puts "#{numero} x 10 = #{numero * 10}"
